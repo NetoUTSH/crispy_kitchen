4db43556cad6c84263cc23d5b61b02e7a6337c01
Jos278 <164915605+Jos278@users.noreply.github.com>
2025-07-09 10:57:04 -0600

    ADD start carousel

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { FormComponent } from './form/components/formComponent'
+import { Carousel_component } from "./core/components/carrusel/Carrusel";
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <FormComponent/>
+      <Carousel_component />
     </>
   )
 }
---
+++ src/core/components/carrusel/Carrusel.jsx
@@ -0,0 +1,55 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { useState } from "react";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./Carrusel.css";
+
+export const Carousel_component = () => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleNext = () => {
+    const newIndex = (selectedIndex + 1) % slides.length;
+    setSelectedIndex(newIndex);
+  };
+
+  const handlePrev = () => {
+    const newIndex = (selectedIndex - 1 + slides.length) % slides.length;
+    setSelectedIndex(newIndex);
+  };
+
+  const slides = [
+    { image: "", alt: "Slide 1" },
+    { image: "", alt: "Slide 2" },
+    { image: "", alt: "Slide 3" },
+  ];
+
+  return (
+    <div className="contenedor-carrusel">
+      <Carousel
+        selectedItem={selectedIndex}
+        showArrows={false}
+        autoPlay={false}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        showIndicators={false}
+        onChange={(index) => setSelectedIndex(index)}
+      >
+        {slides.map((slide, i) => (
+          <div key={i} className="slide-derecha">
+            <img src={slide.image} alt={slide.alt} />
+          </div>
+        ))}
+      </Carousel>
+
+      <div className="botones">
+        <button onClick={handlePrev} className="boton-anterior">
+          <i className="bi bi-arrow-up-left"></i>
+        </button>
+        <button onClick={handleNext} className="boton-siguiente">
+          <i className="bi bi-arrow-up-right"></i>
+        </button>
+      </div>
+    </div>
+  );
+};
