065bf6872947d8b5d589c1ddead831821097f9f5
daeeraleylani <dairaleylani@gmail.com>
2025-07-16 14:13:33 -0600

    Se modifico app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { FormComponent } from './core/calendly-form/components/FormComponent'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import { ViewLandindPageComponent } from './core/landing-page/components/ViewLandingPage/ViewLandindPageComponent'
+import Card from './core/components/card/cardComponet'
 
 function App() {
   return (
     <>
-      
+      <Card />
     <Router>
       <Routes>
         <Route path="/form" element={<FormComponent/>}/>
