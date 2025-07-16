936019bf79e9a89bf8c6cb4f14148629110aa4af
FrancoSG13 <franciscosg1310@gmail.com>
2025-07-16 15:21:43 -0600

    [FIX] correccion de css y adicion de nuevos estilos

--- src/core/components/footer/Footer.jsx
+++ src/core/components/footer/Footer.jsx
@@ -3,37 +3,43 @@ import "./footer.css";
 export const Footer = () => {
   return (
     <>
-      <footer class="footer">
-  <div class="footer-content">
-    <div class="footer-section location">
-      <h2>Crispy Kitchen</h2>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+<footer className="footer">
+  <div className="footer-container">
+  <h2 className="footer-title">Crispy Kitchen</h2>
+  <div className="footer-content">
+    <div className="footer-section location">
       <h3>Location</h3>
       <p>121 Einstein Loop N, Bronx, NY 10475, United States</p>
       <button>Directions</button>
     </div>
 
-    <div class="footer-section hours">
+    <div className="footer-section hours">
       <h3>Opening Hours</h3>
       <p>Monday - Friday</p>
       <p>10:00 AM - 08:00 PM</p>
       <p>Tel: 010-02-0340</p>
     </div>
 
-    <div class="footer-section social">
+    <div className="footer-section social">
       <h3>Social</h3>
-      <div class="social-icons">
-        <a href="#"><i class="fab fa-facebook-f"></i></a>
-        <a href="#"><i class="fab fa-instagram"></i></a>
-        <a href="#"><i class="fab fa-twitter"></i></a>
-        <a href="#"><i class="fab fa-youtube"></i></a>
+      <div className="social-icons">
+        <a href="#"><i className="fab fa-facebook-f"></i></a>
+        <a href="#"><i className="fab fa-instagram"></i></a>
+        <a href="#"><i className="fab fa-twitter"></i></a>
+        <a href="#"><i className="fab fa-youtube"></i></a>
       </div>
-    </div>
-  </div>
 
-  <div class="footer-bottom">
+  <div className="footer-bottom">
     <p>Copyright © 2022 Crispy Kitchen Co., Ltd.</p>
   </div>
+
+    </div>
+  </div>
+</div>
 </footer>
+
+
 
     </>
   )
--- src/core/landing-page/components/ViewLandingPage/ViewLandindPageComponent.jsx
+++ src/core/landing-page/components/ViewLandingPage/ViewLandindPageComponent.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navbar } from '../../../components/navbar/Navbar'
+import { Footer} from '../../../components/footer/Footer'
 
 export const ViewLandindPageComponent = () => {
   return (
 <>
     <Navbar></Navbar>
+    <Footer></Footer>
 </>
   )
 }
